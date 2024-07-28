9450f60c3a80c53004f1c7f2845f063c9b789ce3
Gricay-vasily <vasilygr666@gmail.com>
2024-07-28 14:25:18 +0300

    Упорядкування структури

--- app.py
+++ app.py
@@ -3,15 +3,21 @@
 '''
 
 import streamlit as st
-import tensorflow as tf
 import numpy as np
 
 from PIL import Image
 from tensorflow.keras.models import load_model
 
 # Завантаження моделей
-generator = load_model("generator.h5")
-discriminator = load_model("discriminator.h5")
+def load_models():
+    '''Завантаження моделей'''
+    # gen_file = "generator.h5"
+    gen_file = "generator_model.h5"
+    # disc_file = "discriminator.h5"
+    disc_file = "disc_model.h5"
+    gen = load_model(gen_file)
+    disc = load_model(disc_file)
+    return gen, disc
 
 # Розмір латентного простору
 LATENT_DIM = 100
@@ -23,11 +29,12 @@ def init_states():
     '''
     st.session_state.loaded_image = None
     st.session_state.generated_image = None
-    
+
 def load_image():
     '''Зававнтаження зображення'''
     l_f = st.file_uploader(label="Виберіть або перетягніть малюнок",
                                     type = ["png", "jpg", "jpeg"])
+    init_states()
     if l_f is None:
         init_states()
     return l_f
@@ -51,25 +58,48 @@ def prepare_image(img):
     img = np.expand_dims(img, axis=0)
     return img
 
+def gen_show_image(gen, disc, latent_dim):
+    '''Генерація зображення'''
+    noise = np.random.normal(0, 1, (1, latent_dim))
+    generated_img = gen.predict(noise)
+    # Відображення згенерованого зображення
+    st.session_state.generated_image = Image.fromarray(generated_img[0], "RGB")
+    show_image(img=st.session_state.generated_image,
+                caption="Згенероване зображення",
+                height=100)
+    show_predictions(disc=disc,
+                     img=generated_img,
+                     img_class="згенерованого")
 
 def show_image(img, caption = "",height = 100):
     '''
     Виведення зображення з деяким його нормуванням за розмірами
     '''
-    # Відкриття зображення за допомогою Pillow
     # Приведення розміру до стандартного по висоті 100
     st.write(f"Реальний розмір зображення : {img.height} x {img.width}")
     koef = height / img.height
     img = img.resize((
         int(img.width * koef), int(img.height * koef)
                       ))
-    st.write(f"Зменшений розмір зображення : {img.height} x {img.width}")
+    st.write(f"Перетворений розмір зображення : {img.height} x {img.width}")
     st.image(image=img,
                  caption=caption)
+
+def show_predictions(disc, img, img_class:str):
+    '''Виведення результатів роботи дискримінатора'''
+    prediction = disc.predict(img)
+    predicted_class = "Real" if prediction >= 0.5 else "Fake"
+    probability = prediction[0][0]
+    st.write(f"Передбачений клас для {img_class} зображення: {predicted_class}")
+    st.write(f"Імовірність вірної класифікації для {img_class} зображення: {probability:.2f}")
+
+
 
 # ----------------------------------------------------------------------------
 # Вхід в головну частину програми
 if __name__ == "__main__":
+    # Завантаження моделей
+    generator, discriminator = load_models()
 
     # Ініціалізація початкових станів
     if "loaded_image" not in st.session_state:
@@ -83,8 +113,10 @@ if __name__ == "__main__":
     # Завантаження зображення
     loaded_file = load_image()
 
-    st.button("Підтвердіть вибір", on_click=set_image(loaded_file))
-    # st.button("Скинути всі зображення", on_click=init_states())
+    st.button("Підтвердіть вибір",
+              key="Real_Img")
+    if st.button:
+        set_image(loaded_file)
 
     if st.session_state.loaded_image is not None:
         show_image(img=st.session_state.loaded_image,
@@ -94,30 +126,11 @@ if __name__ == "__main__":
         # Підготовка зображення
         prepared_image = prepare_image(st.session_state.loaded_image)
 
-    # Класифікація завантаженого зображення
-    prediction = discriminator.predict(prepared_image)
-    predicted_class = "Real" if prediction >= 0.5 else "Fake"
-    probability = prediction[0][0]
+        # Класифікація завантаженого зображення
+        show_predictions(disc=discriminator, img=prepared_image, img_class="справжнього")
 
-    st.write(f"Predicted Class: {predicted_class}")
-    st.write(f"Probability: {probability:.2f}")
-
-    # Генерація нового зображення
-    noise = np.random.normal(0, 1, (1, LATENT_DIM))
-    generated_img = generator.predict(noise)
-
-    # Денормалізація згенерованого зображення
-    generated_img = 0.5 * generated_img + 0.5
-    generated_img = np.clip(generated_img, 0, 1)
-
-    # Відображення згенерованого зображення
-    st.session_state.generated_image = generated_img
-    st.image(generated_img[0], caption="Generated Image", use_column_width=True)
-
-    # Перевірка згенерованого зображення дискримінатором
-    gen_prediction = discriminator.predict(generated_img)
-    gen_predicted_class = "Real" if gen_prediction >= 0.5 else "Fake"
-    gen_probability = gen_prediction[0][0]
-
-    st.write(f"Generated Image Predicted Class: {gen_predicted_class}")
-    st.write(f"Generated Image Probability: {gen_probability:.2f}")
+        # Генерація нового зображення
+        st.title("Генерація зображення")
+        st.button("Згенерувати зображення",
+                  key="Gen_Img",
+                  on_click=gen_show_image(generator, discriminator, LATENT_DIM))
